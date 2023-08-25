9a7f549acfdc82e964edda3699c03399516b9674
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-08-24 22:46:06 -0300

    I did the GetMapping exercises

--- src/main/java/com/firstspringboot/proyect/controller/HelloController.java
+++ src/main/java/com/firstspringboot/proyect/controller/HelloController.java
@@ -18,11 +18,17 @@ public class HelloController {
         return "Bienvenidooo " + nombre + "!!!";
     }
     
-     @GetMapping("/hola/{nombre}/{edad}/{profesion}")
+    @GetMapping("/hola/{nombre}/{edad}/{profesion}")
     public String describeYou(@PathVariable String nombre,
                              @PathVariable int edad,
                              @PathVariable String profesion){
         return "Hola! Me llamo " + nombre + ", tengo " + edad + " años y soy " + profesion;
+    }
+    
+     @GetMapping("/sumar/{num1}/{num2}")
+    public String sumar(@PathVariable int num1, @PathVariable int num2) {
+        int suma = num1 + num2;    
+        return "El resultado de la suma de " + num1 + " y " + num2 + " es: " + suma;
     }
     
     @GetMapping("/hola")
@@ -35,5 +41,22 @@ public class HelloController {
                              @RequestParam int edad,
                              @RequestParam String profesion){
         return "Hola! Me llamo " + nombre + ", tengo " + edad + " años y soy " + profesion;
+    }
+    
+     @GetMapping("/imc")
+    public String calcularIMC(@RequestParam float imc){
+        String respuesta;
+        
+        if (imc < 1) {
+            respuesta = "El valor ingresado para el IMC es incorrecto";
+        } else if (imc < 18.6) {
+            respuesta = "Peso insuficiente";
+        } else if (imc < 25) {
+            respuesta = "Peso normal";
+        } else if (imc < 30) {
+            respuesta = "Sobrepeso";
+        } else respuesta = "Obesidad";
+        
+        return respuesta;
     }
 }
